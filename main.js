fcc85e3ffc1089faadf065a4be2338639e23ea90
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2024-03-09 09:00:49 +0900

    Add index.html styles.css main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+const myLibrary = [];
+
+const formButton = document.querySelector(".form-btn");
+const bookForm = document.getElementById("book-form");
+formButton.addEventListener('click', () => {
+  if( bookForm.style.display ==='block'){
+    bookForm.style.display = 'none';
+  }else {
+    bookForm.style.display = 'block';
+
+  }
+  
+});
+
+
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = Boolean(read);
+
+  this.info = function() {
+    return 'info';
+  }
+
+  Book.prototype.toRead = function() {
+    this.read = true;
+  }
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+
+
+const lordOfTheRings = new Book('Lord of the rings', 'a nerd guy', 1111, false);
+
+console.log(lordOfTheRings.read);
+lordOfTheRings.toRead();
+console.log(lordOfTheRings.read);
