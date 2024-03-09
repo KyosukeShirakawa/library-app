0313897a14fec88ec533dbd720ae7dc17d70e51d
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2024-03-09 20:48:46 +0900

    Made the form work with js

--- main.js
+++ main.js
@@ -1,25 +1,90 @@
 const myLibrary = [];
 
 
+document.addEventListener('DOMContentLoaded', () => {
 
-const bookContainer = document.querySelector(".book-container");
-const formButton = document.querySelector(".form-btn");
-const formContainer = document.getElementById("form-container");
+  const formButton = document.querySelector(".form-btn");
+  const formContainer = document.getElementById("form-container");
+  const submitBtn = document.querySelector(".submit-btn");
+  const bookTitle = document.querySelector("#book-title");
+  const bookAuthor = document.querySelector("#book-author");
+  const bookPage = document.querySelector("#book-page");
+  const isRead = document.getElementsByName("is-read");
+  const bookContainer = document.querySelector(".book-container");
+  const removeBtns = document.querySelectorAll(".remove-btn")
+
+  formButton.addEventListener('click', () => {
+    if(formContainer.style.display ==='block'){
+      formContainer.style.display = 'none';
+    }else {
+      formContainer.style.display = 'block';
+    }
+  });
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPage.value;
+    let read =  isRead.value;
+    let newBook = new Book(title, author, pages, read);
+    console.log(`this is a new book: ${newBook}`);
+    addBookToLibrary(newBook);
+    displayBooks();
+ 
+  });
+
+  bookContainer.addEventListener('click', (e) => {
+    if(e.target.classList.contains('remove-btn')){
+      console.log(`i pop this ${myLibrary.pop()}`);
+    }
+  });
 
 
 
+  function displayBooks() {
+    myLibrary.forEach((book) => {
+      let card = document.createElement('div');
+      let title = document.createElement('p');
+      let author = document.createElement('p');
+      let pages = document.createElement('p');
+      let read = document.createElement('p');
+      let removeBtn = document.createElement('button');
+      
 
-formButton.addEventListener('click', () => {
-  if(formContainer.style.display ==='block'){
-    formContainer.style.display = 'none';
-  }else {
-    formContainer.style.display = 'block';
 
+      title.textContent = `Title: ${book.title}`;
+      author.textContent = `Author: ${book.author}`;
+      pages.textContent = `No. of Pages ${book.pages}`;
+      read.textContent = `Have you read this book?: ${book.isRead}`;
+      removeBtn.textContent = 'Remove';
+
+
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+      card.appendChild(read);
+      card.appendChild(removeBtn);
+
+      removeBtn.classList.add('remove-btn');
+      card.classList.add('card');
+
+  
+      bookContainer.appendChild(card);
+      
+      console.log(book);
+  
+    });
   }
   
+
 });
 
 
+
+
+
+
+// Book object
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -30,27 +95,21 @@ function Book(title, author, pages, read) {
     return 'info';
   }
 
+
   Book.prototype.toRead = function() {
     this.read = true;
   }
 }
+
+
+// book functions
 
 function addBookToLibrary(book) {
   if(!myLibrary.includes(book))
    myLibrary.push(book);
 }
 
-function displayBooks() {
-  myLibrary.forEach((book) => {
-    console.log(book);
-
-  });
-}
 
 
 
-const lordOfTheRings = new Book('Lord of the rings', 'a nerd guy', 1111, false);
 
-console.log(lordOfTheRings.read);
-lordOfTheRings.toRead();
-console.log(lordOfTheRings.read);
