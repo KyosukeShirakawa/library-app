07d2ef1d92c2d3af85a04fa2c83325158fbca0c5
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2024-03-19 20:22:57 +0900

    Organize functions

--- main.js
+++ main.js
@@ -1,155 +1,117 @@
-const myLibrary = [];
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  const formButton = document.querySelector(".form-btn");
-  const formContainer = document.getElementById("form-container");
-  const submitBtn = document.querySelector(".submit-btn");
-  // const bookTitle = document.querySelector("#book-title");
-  // const bookAuthor = document.querySelector("#book-author");
-  // const bookPage = document.querySelector("#book-page");
-  // const isRead = document.getElementsByName("is-read");
-  const bookContainer = document.querySelector(".book-container");
- 
-  //Hide or show the new book form
-  formButton.addEventListener('click', () => {
-    if(formContainer.style.display ==='block'){
-      formContainer.style.display = 'none';
-    }else {
-      formContainer.style.display = 'block';
-    }
-  });
-
-  //Submit the form and add a new book
-  submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    //Create variables
-    let title = document.getElementById("book-title").value;
-    let author = document.getElementById("book-author").value;
-    let pages = document.getElementById("book-page").value;
-    let read;
-    if(document.querySelector('input[name="is-read"]:checked').value==='yes'){
-       read = true;
-    }else if (document.querySelector('input[name="is-read"]:checked').value==='no'){
-       read = false;
-    }
-    
-
-    
-    
-    //Create a new Book
-    let newBook = new Book(title, author, pages, read);
-    //Add the book to the array
-    newBook.addBookToLibrary(newBook);
-    
-    //display the books in myLibrary
-    displayBooks();
- 
-  });
-
-
-
-
-  function displayBooks() {
-    myLibrary.forEach((book) => {
-
-      //Create HTML elements
-      let card = document.createElement('div');
-      let title = document.createElement('p');
-      let author = document.createElement('p');
-      let pages = document.createElement('p');
-      let read = document.createElement('p');
-      let removeBtn = document.createElement('button');
-      
-
-      //add text to the elements
-      title.textContent = `Title: ${book.title}`;
-      author.textContent = `Author: ${book.author}`;
-      pages.textContent = `No. of Pages: ${book.pages}`;
-      read.textContent = `Have you read this book?: ${book.read}`;
-      removeBtn.textContent = 'Remove';
-
-      //Store in a card
-      card.appendChild(title);
-      card.appendChild(author);
-      card.appendChild(pages);
-      card.appendChild(read);
-      card.appendChild(removeBtn);
-
-      //Assign CSS class 
-      removeBtn.classList.add('remove-btn');
-      card.classList.add('card');
-      
-
-
-
-      //Put the card to the div container
-      bookContainer.appendChild(card);
-  
-    });
-  }
-  
-
-  //Remove button to remove a book
-
-  bookContainer.addEventListener('click', (e) => {
-    if(e.target.classList.contains('remove-btn')){
-      // console.log(`i pop this ${myLibrary.pop()}`);
-
-
-      console.log(myLibrary);
-      console.log("removing a book");
-      removeBookFromLibrary();
-      console.log(myLibrary);    
-    }
-  });
-
-
-});
-
-
-
-
 
 
 // Book object
 class Book {
-    Book (title, author, pages, read) {
+  constructor (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-
-    this.info = function() {
-      return 'info';
-    }
-
-
-    Book.prototype.toRead = function() {
-      this.read = true;
-    }
-  }
-
-
-  // book functions
-
-   addBookToLibrary(book) {
-    if(!myLibrary.includes(book)){
-    myLibrary.push(book);
-    console.log(myLibrary);
-
-    }
-
-  }
-
-   removeBookFromLibrary(book){
-    if(myLibrary.includes(book)){
-      myLibrary.pop(book);
-    }
-
+    this.read = read; 
   }
 
 }
+
+
+
+const myLibrary = (function() {
+
+  const library = [];
+
+  //cache DOM
+  const bookContainer = document.querySelector('#book-container')
+  const newFormBtn = document.querySelector('#form-btn')
+  const form = document.querySelector('#book-form');
+  const submitBtn= document.querySelector('#submit-btn');
+
+  //bind events
+  newFormBtn.addEventListener('click',toggleForm);
+  submitBtn.addEventListener('click', handleSubmittedInfo);
+//   <!-- <div class="card">
+//   <p class="title">Title: fwefewf</p>
+//   <p class="author">Author: ewfref</p>
+//   <p class="pages">No. of pages: 332</p>
+//   <p class="is-read">Have you read this book?: Yea</p>
+//   <button class="remove-btn">Remove</button>
+// </div> -->
+
+
+  function render() {
+    bookContainer.innerHTML = '';
+    library.forEach((book) => {
+      let card = document.createElement('div');
+      card.setAttribute('class','card');
+
+      let title = document.createElement('p');
+      title.setAttribute('class','title');
+      title.textContent = `Title: ${book.title}`
+
+      let author = document.createElement('p');
+      author.setAttribute('class','author');
+      author.textContent = `Author: ${book.author}`
+
+      let pages = document.createElement('p');
+      pages.setAttribute('class','pages');
+      pages.textContent = `No. of pages: ${book.pages}`
+
+      let read = document.createElement('p');
+      read.setAttribute('class','is-read');
+      read.textContent = `Have you read this?: ${book.read}`
+
+      let removeBtn = document.createElement('button')
+      removeBtn.setAttribute('class','remove-btn');
+      removeBtn.textContent = "Remove"
+
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+      card.appendChild(read);
+
+      bookContainer.appendChild(card);
+    })
+  }
+
+
+  function toggleForm() {
+    return (form.style.display=='none') ? (form.style.display = 'block') : (form.style.display = 'none'); 
+  }
+
+
+  function handleSubmittedInfo(e) {
+    e.preventDefault();
+    let title = document.querySelector('#book-title').value;
+    let author = document.querySelector('#book-author').value;
+    let pages = document.querySelector('#book-page').value;
+    let read = document.querySelector('input[name="is-read"]:checked').value;
+
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook);
+    addBookToLibrary(newBook);
+    form.reset();
+    render();
+
+  }
+
+  function addBookToLibrary(book) {
+    if(!library.includes(book)){
+      library.push(book);
+      console.log(library);
+    }
+  }
+
+
+
+    // function removeBookFromLibrary(book){
+    //   if(myLibrary.includes(book)){
+    //     myLibrary.pop(book);
+    //   }
+  
+    // }
+
+
+
+})();
+
+
+
+
 
